22fb937d658d919d75d1160e3352bf2c9e863d36
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-11-02 22:09:41 -0700

    Create connnect.py

---
+++ AUVSI_System/controlcenter/connnect.py
@@ -0,0 +1,11 @@
+from auvsi_suas.client import client
+from auvsi_suas.proto import interop_api_pb2
+
+def connect(URL,Userame,Password):
+    client = client.Client(url=URL,
+                        username=Userame,
+                        password=Password)
+    teams = client.get_teams()
+    mission = client.get_mission(1)
+    print(teams)
+    print(mision)
